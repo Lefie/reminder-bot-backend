6d66b70d8f1f1e665b0ca6b78bd8de982288ef3e
Lefie <qiwenruan11@gmail.com>
2025-03-16 15:55:16 -0400

    minor update

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express()
 
 
 
-const allowedOrigins = ["http://localhost:5500","http://127.0.0.1:5500"]
+const allowedOrigins = ["http://localhost:5500","http://127.0.0.1:5500","https://reminder-bot-frontend.vercel.app/"]
 
 // cors enabled
 app.use(cors({
