81fb6e69f6ddcc0b0eb8c7acb0ef329dbe73e517
Lefie <qiwenruan11@gmail.com>
2025-07-22 13:39:00 -0400

    cors

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express()
 
 weeklyEmailReminder()
 
-const allowedOrigins = ["http://localhost:5500","http://127.0.0.1:5500","http://localhost:5173","https://reminder-bot-frontend.vercel.app"]
+const allowedOrigins = ["http://localhost:5500","http://127.0.0.1:5500","http://localhost:5173","https://reminder-bot-sigma.vercel.app/"]
 
 // cors enabled
 app.use(cors({
